86353ea2786d6461e8264c73a67904b8f30e8fe2
OSS_msgil <21700062@handong.edu>
2019-05-20 07:06:09 +0900

    third commit-assign tokens(nested and size)

--- jsonparser.c
+++ jsonparser.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 char *append(char *str, char ch);
-int token_assigner (int ti, int tj);
+int token_assigner (int ti, int tj, char *str, char **arr, int *size);
 
 int main(void) {
     //invoke variables
   FILE *fp = NULL;
   char ch;
   char *str = (char *)malloc (1048);
-  int i=0, j=0;
+  int i=0, j=0, k=0;
   //open file
   fp = fopen("examples.json","r");
   if (fp == NULL) {
@@ -27,7 +27,7 @@ int main(void) {
   //allocate 2d array
     int MAX_LENGTH = 2048;
     char **arr;
-
+    int size[1000] = {0};
     arr = (char**) malloc(sizeof(char*) * MAX_LENGTH);
     for(int i=0; i<MAX_LENGTH; i++) {
         arr[i] = (char*) malloc (1048);
@@ -41,27 +41,45 @@ int main(void) {
             for (;str[i]!= '"'; i++){
                 arr[j] = append(arr[j],str[i]);
             }
+            if (str[i+1]==':') size[j] = 1; 
+            else size[j] = 0;
             j++;
         }
        //decipher if current string is object
         else if (str[i] == '{'){
+            k=j;
             do{
                 arr[j]= append(arr[j], str[i]);
+                if (str[i] != '{'){
+                k=token_assigner (i,k, str, arr, size);
+                }
                 i++;
+            if (str[i]== ',') size[j]++;
             } while (str[i-1]!= '}');
-            j++;
+            j= ++k;
         }
         //decipher if current string is array
         else if (str[i] =='['){
+            k=j;
+            size[j]++;
             for (;str[i-1]!=']'; i++){
                 arr[j]=append(arr[j],str[i]);
+                if (str[i]!= '['){
+                k=token_assigner (i,k,str,arr, size);
+                }
+                if (str[i]== ',') size[j]++;
             }
-            j++;
+            j=++k;
         }
         //
         i++;
     }
-    printf("%s\n", arr[13]);
+    // print tokens
+   for (i=0; i<21; i++){
+    printf("[%d]%s", i+1,arr[i]);
+    printf("  (size = %d)\n", size[i]);
+    }
+
 // close files and free mallocated items.
   fclose(fp);
   free(str);
@@ -80,3 +98,44 @@ char *append(char *str, char ch){
   str[len+1]= '\0';
   return str;
 }
+//assign nested tokens
+int token_assigner (int ti, int tj, char *str, char**arr, int *size){
+    int tk =0;
+    if (str[ti] == ',') return tj;
+    if (str[ti-1] == '"' && str[ti] != ':' && str[ti] != '\n'){
+            tj++;
+            for (;str[ti] != '"'; ti++){
+                arr[tj] = append(arr[tj],str[ti]);
+            }
+            if (str[ti+1]==':') size[tj] = 1; 
+            else size[tj] = 0;
+        }
+     else if (str[ti] == '{'){
+            tj++;
+            tk=tj;
+            do{
+
+                arr[tj]= append(arr[tj], str[ti]);
+                if (str[ti] != '{'){
+                tk=token_assigner(ti, tk,str,arr, size);
+                }
+                ti++;
+        if (str[ti]== ',') size[tj]++;
+            } while (str[ti-1]!= '}');
+            tj=++tk;
+        }
+    else if (str[ti] =='['){
+            tk=tj;
+            tj++;
+            for (;str[ti-1]!=']'; ti++){
+                arr[tj]=append(arr[tj],str[ti]);
+                if (str[ti] != '['){
+                tk=token_assigner(ti, tk,str,arr, size);
+                }
+             if (str[ti]== ',') size[tj]++;
+            }
+            tj=++tk;
+        }
+        return tj;
+}
+	
